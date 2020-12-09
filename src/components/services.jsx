fbda2e5d155fff187320e9b41bac3d3784830515
Hudz <rshudz@gmail.com>
2020-12-09 16:34:19 +0200

    landing page with react and bootstrap

---
+++ src/components/services.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export class Services extends Component {
+  render() {
+    return (
+      <div id="services" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>Services</h2>
+            <p>
+              Some types of services
+            </p>
+          </div>
+          <div className="row">
+            {this.props.data
+              ? this.props.data.map((d, i) => (
+                  <div  key={`${d.name}-${i}`} className="col-md-4">
+                        {" "}
+                        <img src={d.img} alt="" />{" "}
+                    <div className="service-desc">
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
+                  </div>
+                ))
+              : "loading"}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Services;
